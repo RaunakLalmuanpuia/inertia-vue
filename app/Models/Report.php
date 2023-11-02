e8daec8a20f04d5ca3402d45454ea0acfa3e0317
Raunak Msegs <Raunak.msegs@gmail.com>
2023-11-02 15:34:51 +0530

    first commit

---
+++ app/Models/Report.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Report extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'status',
+        'feedback',
+        'employee_id',
+        'file_path',
+        'employer_id'
+
+    ];
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
